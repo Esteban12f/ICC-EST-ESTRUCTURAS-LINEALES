ce46ea49a055f5fada197dc4026e748365b4b742
Esteban12f <joehv33@gmail.com>
2024-12-04 23:29:14 -0500

    Actualizado

--- src/App.java
+++ src/App.java
@@ -1,5 +1,30 @@
+import Materia.Stacks.Stack;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        runStack();
+    }
+
+    public static void runStack(){
+
+        Stack stack = new Stack();
+
+        stack.push(5);
+        stack.push(7);
+        stack.push(10);
+        stack.push(20);
+
+        stack.printStack();
+        int cont = stack.getSize();
+        System.out.println("Tamanio: " + cont);
+
+        System.out.println(stack.getSize2());
+
+
+        System.out.println("Cima    ->" + stack.peek());
+        System.out.println("Retirar ->" + stack.pop());
+        System.out.println("Cima    ->" + stack.peek());
+        System.out.println("Retirar ->" + stack.pop());
+        System.out.println("Cima    ->" + stack.peek());
     }
 }
