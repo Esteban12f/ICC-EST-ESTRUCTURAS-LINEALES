9b2b4254bf10b0d29a4148fb943c2c785f9750f5
Esteban12f <joehv33@gmail.com>
2024-12-06 12:11:21 -0500

    Add: Generic Stacks

--- src/App.java
+++ src/App.java
@@ -1,8 +1,11 @@
 import Materia.Stacks.Stack;
+import Materia.Stacks.StackGeneric;
+import models.Pantalla;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        runStack();
+        //runStack();
+        runStackGeneric();
     }
 
     public static void runStack(){
@@ -27,4 +30,22 @@ public class App {
         System.out.println("Retirar ->" + stack.pop());
         System.out.println("Cima    ->" + stack.peek());
     }
+
+    public static void runStackGeneric(){
+        StackGeneric<Pantalla> router = new StackGeneric<>();
+        router.push(new Pantalla(1, "Home Page", "/home"));
+        router.push(new Pantalla(1, "Menu Page", "/home/menu"));
+        router.push(new Pantalla(1, "Users Page", "/home/menu/users"));
+
+        System.out.println("Estoy en " + router.peek().getRuta());
+        System.out.println("Regreso a " + router.popNode().getNext().getValue().getRuta());
+        System.out.println("Estoy en " + router.peek().getRuta());
+
+        router.push(new Pantalla(1, "Settings Page", "/home/menu/settings"));
+        
+        System.out.println("Pantallas = " + router.getSize());
+        System.out.println("Estoy en " + router.peek().getRuta());
+        router.printStack();
+    }
+
 }
