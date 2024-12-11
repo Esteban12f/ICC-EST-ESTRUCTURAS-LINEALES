45dea6c74d73c117ba92e72af8cc0837c8e5ba65
Esteban12f <joehv33@gmail.com>
2024-12-11 12:19:31 -0500

    ADD: Ejercicio LinkedList

--- src/Controllers/ContactManager.java
+++ src/Controllers/ContactManager.java
@@ -1,5 +1,6 @@
 package Controllers;
 
+import Materia.Models.NodeGeneric;
 import models.Contact;
 import models.LinkedList;
 
@@ -18,14 +19,41 @@ public class ContactManager {
 
 
     public Contact<?,?> findContactByName (String name){
+        NodeGeneric<Contact<?,?>> current = contacts.getHead();
+        while (current != null) {
+            if ( ((String)(current.getValue().getName())).equalsIgnoreCase(name) ){
+                return current.getValue();
+            }
+            current = current.getNext();
+        }
+
         return null;
     }
 
     public void deleteContactByName (String name){
+        NodeGeneric<Contact<?,?>> current = contacts.getHead();
 
+        if (current == null) return;
+
+        // CASO 1 que el contacto sea la HEAD
+        if ( ((String)(current.getValue().getName())).equalsIgnoreCase(name) ) {
+            contacts.setHead(current.getNext());
+            contacts.setSize(contacts.getSize() - 1);
+            return;
+        }
+
+        // CASO 2 que el contacto este en el resto
+        while (current.getNext() != null) {
+            if (((String)(current.getNext().getValue().getName())).equalsIgnoreCase(name)) {
+                current.setNext(current.getNext().getNext());
+                contacts.setSize(contacts.getSize() - 1);
+                return;
+            }
+            current = current.getNext();
+        }
     }
 
-    public void prinyList(){
+    public void printList(){
         contacts.print();
     }
 }
